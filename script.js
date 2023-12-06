7a8d35ca183553a04255f3faf094b4aec131b2c5
Amangupta0918 <68322087+Amangupta0918@users.noreply.github.com>
2023-12-06 16:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
-const mul = (let val1, val2) => {
-	let sum = val1*2;
-	let ans = sum + val2;
-	return ans;
-}
-const val1 = prompt("Enter value 1");
-const val2 = prompt("Enter value 2");
-alert(mul(val1,val2));
+let num1;
+num1 = prompt("Enter the first number:");
+			num1 = parseFloat(num1);
+			let result = num1 * 2;
+			let num2;// Get the input for the second variable using the prompt() function
+			num2 = prompt("Enter the second number:");
+			num2 = parseFloat(num2);
+				result += num2;
+			alert("The final result is: " + result);
